93d7bbe77df7fc35563fc6dd980f78745b9afc1a
KimNamKwang <durante0916@gmail.com>
2023-02-05 20:33:33 +0900

    sdasf

---
+++ docs/js/new.js
@@ -0,0 +1,5 @@
+document.getElementById("btnA").addEventListener("click", function () {
+  //   document.getElementById("text_for_reserv_check_and_cancel").style.display =
+  //     "block";
+  document.getElementById("boxB").style.opacity = "1";
+});
--- docs/js/termsForHomes.js
+++ docs/js/termsForHomes.js
@@ -1,30 +1,57 @@
-function targetText(
+document
+  .getElementById("reserv_check_and_cancel")
+  .addEventListener("click", function () {
+    greenBorderForReservCheckAndCancel;
+    showTextForReservCheckAndCancel();
+    hideAgreementBox();
+  });
+
+function showTextForReservCheckAndCancel(
   event /**event는 기본적으로 들어가는 파라미터이다. 어떤 것이든 입력을 의미 */
 ) {
-  let queryTargetText = document.querySelector("#agreement_box");
+  document.getElementById("text_for_reserv_check_and_cancel").style.display =
+    "block";
+  // agreeBox = document.getElementById("agreement_box");
+  document.getElementById("agreement_box").style.display = "none";
+}
+
+function hideAgreementBox(
+  event /**event는 기본적으로 들어가는 파라미터이다. 어떤 것이든 입력을 의미 */
+) {
+  let queryTargetText = document.querySelector("#boxForChange");
   let queryChangeText = document.getElementById(
     "#text_for_reserv_check_and_cancel"
   );
   queryTargetText.innerHTML = queryChangeText;
-  let changeOpacity = document.getElementById("abc");
-  x.style.fontSize = "25px";
-  x.style.color = "blue";
+}
+
+function greenBorderForReservCheckAndCancel(
+  event /**event는 기본적으로 들어가는 파라미터이다. 어떤 것이든 입력을 의미 */
+) {
+  document.getElementById("reserv_check_and_cancel").style.borderBottom =
+    "3px solid rgb(55, 210, 67)";
+}
+
+document
+  .getElementById("reserv_for_home")
+  .addEventListener("click", function () {
+    showAgreementBox();
+    // changeToGeneralReserv();
+  });
+
+function showAgreementBox(
+  event /**event는 기본적으로 들어가는 파라미터이다. 어떤 것이든 입력을 의미 */
+) {
+  document.getElementById("agreement_box").style.display = "block";
+  document.getElementById("boxForChange").style.display = "none";
+  document.getElementById("text_for_reserv_check_and_cancel").style.display =
+    "none";
 }
 
 function changeToGeneralReserv(event) {
-  let queryTargetText = document.querySelector("#agreement_box");
-  let queryChangeText = document.getElementById(
+  let queryTargetText = document.querySelector(
     "#text_for_reserv_check_and_cancel"
   );
-  queryTargetText.queryTargetText.innerHTML = queryChangeText;
-  let changeOpacity = document.getElementById("abc");
-  x.style.fontSize = "25px";
-  x.style.color = "blue";
+  let queryChangeText = document.getElementById("#agreement_box");
+  queryTargetText.innerHTML = queryChangeText;
 }
-
-// $(document).ready(function () {
-//   //with ID
-//   $("#reserv_check_and_cancel").click(function () {
-//     $("#reserv_for_home").css({ opacity: "50" });
-//   });
-// });
