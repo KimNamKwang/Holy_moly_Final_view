3d4bfbfbcf52862ff07c558239d70e6d5f6d5000
KimNamKwang <durante0916@gmail.com>
2023-02-18 22:13:18 +0900

    후

--- Databases/merge_DML.sql
+++ Databases/merge_DML.sql
@@ -68,8 +68,12 @@ VALUES
 INSERT INTO PROGRESS_STATUS_TYPE
 (PROGRESS_STATUS_UID,PROGRESS_STATUS_TYPE,PROGRESS_STATUS_DESCRIPTION)
 VALUES
-('PROG_STAT_WAIT','IN_PROGRESS','문의중'),
-('PROG_STAT_DONE','COMPLETION','답변완료')
+('INQ_PROG_STAT_WAIT','IN_PROGRESS','문의중'), 
+('INQ_PROG_STAT_DONE','COMPLETION','답변완료'),
+('SHIP_PROG_STAT_REGI','REGISTER','접수완료'),
+('SHIP_PROG_STAT_PICK','PIPCKUP','수거완료'),
+('SHIP_PROG_STAT_INPROG','IN PROGRESS','배송중'),
+('SHIP_PROG_STAT_COMP','COMPLETE','배송완료')
 ;
 
 INSERT INTO INQUIRYS
@@ -77,7 +81,52 @@ INSERT INTO INQUIRYS
 TRACKING_NUMBER_FOR_INQUIRY,USER_UID,INQUIRY_TYPE_UID,PROGRESS_STATUS_UID)
 VALUES
 ('INQ_230217_1','배송문의드립니다',sysdate(),'어제 발송했는데 며칠이나 걸리는지 문의좀 드릴려고요','네 안녕하세요 고객님...',
-'TRK_20230217_1','user1','Q_SHIP','PROG_STAT_DONE'),
+'TRK_20230217_1','user1','Q_SHIP','INQ_PROG_STAT_DONE'),
 ('INQ_230217_2','설 명절선물 세트 판매 문의',sysdate(),'설 명절선물 세트 구매언제능한가요?',null,
-null,'user2','Q_COMP_ELSE','PROG_STAT_WAIT')
+null,'user2','Q_COMP_ELSE','INQ_PROG_STAT_WAIT')
 ;
+
+INSERT INTO REQUESTS
+(REQUEST_UID, REQUEST_DESCRIPTION)
+VALUES
+('REQ_01','기사님 방문 전 연락주세요'),('REQ_02','보낼 물품을 문 앞에 놓아 두겠습니다'),('REQ_03','보낼 물품을 경비실에 맡겨두겠습니다'),
+('REQ_04','파손에 주의해 주세요연락주세요'),('REQ_05','배송 시 경비실에 맡겨 주세요'),('REQ_06','배송 시 택배함에 넣어 주세요'),
+('REQ_07','배송 시 문앞에 놓아 주세요.')
+;
+
+INSERT INTO ITEM_TYPES
+(ITEM_TYPE_UID, ITEM_TYPE, ITEM_TYPE_DESCRIPTION)
+VALUES
+('item_f_01','food','농축산물류'),('item_f_02','food_dried','건어물'),('item_f_03','food_fruits','과일류'),
+('item_f_04','food_rice','쌀'),('item_f_05','food_vegetables','채소류'),('item_f_06','food_else','기타'),
+
+('item_b_01','book','서적류'),('item_b_02','book_documents','서류'),('item_b_03','book_publications','서적'),
+('item_b_04','book_prints','인쇄물'),('item_b_05','book_else','기타 서적류'),
+
+('item_m_01','medicine','약품류'),('item_m_02','medicine_drugs','의약품'),('item_m_03','medicine_oriental','한약'),
+('item_m_04','medicine_healthSupplement','건강보조제품'),('item_m_05','food','농축산물류'),('item_m_06','food','농축산물류'),
+
+('item_c_01','clothing','의류/잡화'),('item_c_02','clothing_clothes','의류'),('item_c_03','clothing_shoes','신발'),
+('item_c_04','clothing_bagAndWallets','가방/지갑'),('item_c_05','clothing_accessories','액세서리')
+;
+
+INSERT INTO SHIPMENT_TYPES
+(SHIPMENT_TYPE_UID, SHIPMENT_TYPE, SHIPMENT_TYPE_DESCRIPTION)
+VALUES
+('SHIP_GEN','GRNERAL','일반 택배'),('SHIP_BULK','BULF','다량 택배'),('SHIP_PRE','PREMIUN','프리미엄 택배')
+;
+
+INSERT INTO SHIPMENT
+(TRACKING_NUMBER, NUMBER_OF_ITEMS, SENDER_NAME, SENDER_PHONE, DEPARTURE_POSTALCODE, DEPARTURE_ADDRESS,
+DEPARTURE_DETAILADDRESS, DEPARTURE_EXTRAADDRESS, RECIPIENT_NAME, RECIPIENT_PHONE, DESTINATION_POSTALCODE,
+DESTINATION_ADDRESS, DESTINATION_DETAILADDRESS, DESTINATION_EXTRAADDRESS, TOTAL_PRICE, ITEM_PRICE,
+SHIPMENT_PASSWORD, VISITING_DATE,SHIPMENT_TYPE_UID, REQUEST_UID, ITEM_TYPE_UID, PROGRESS_STATUS_UID)
+
+VALUES
+('TRK_20230217_1',2,'홍길동','01045671234','01623','서울시 강남구 테헤란로 14','103호',NULL,'이지금','01014235463',
+'02422','서울시 서초구 서초대로14','101동 304호','서초동',50000,25000,1234,'2023-02-19','SHIP_GEN','REQ_04','item_m_02','SHIP_PROG_STAT_REGI'),
+('TRK_20230217_2',6,'하야시','01023245533','11920','서울시 강남구 봉은사로 10','307호',NULL,'서운령','01188882222',
+'21022','서울시 노원구 동일로 10','1층',NULL,180000,30000,1234,'2023-02-22','SHIP_BULK','REQ_02','item_c_02','SHIP_PROG_STAT_INPROG')
+;
+
+
